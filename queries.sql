2cd7923994ad13ee27b46a3d2f54808bc9dcdb33
andrescab <andcab1293@gmail.com>
2022-05-26 16:06:13 -0700

    m7.4

--- queries.sql
+++ queries.sql
@@ -1,3 +1,10 @@
+DROP TABLE departments CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE titles CASCADE;
+
 -- Creating tables for PH-EmployeeDB
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
@@ -17,12 +24,12 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -35,21 +42,33 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE Titles (
+CREATE TABLE dept_Emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+	
+CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY 	
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, title, from_date, to_date) 	
 );
 	
 SELECT * FROM departments
+SELECT * FROM dept_manager
+SELECT * FROM employees;
 
 SELECT first_name, last_name
 FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
-
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+ 
 -- Retirement eligibility
 SELECT first_name, last_name
 FROM employees
@@ -61,8 +80,10 @@ FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-DROP TABLE dept_manager CASCADE
+DROP TABLE dept_emp CASCADE
 DROP TABLE employees CASCADE
+DROP TABLE dept_manager CASCADE
+DROP TABLE retirement_info CASCADE
 
 -- Number of employees retiring
 SELECT COUNT(first_name)
